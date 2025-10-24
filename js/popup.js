58c90a00662c65388b0efa7bacf114de58ee23e2
nguyenhuy91006-Binisme <nguyenhuy91006@gmail.com>
2025-10-24 22:49:24 +0700

    add sign in,sign up quên mk

---
+++ js/popup.js
@@ -0,0 +1,11 @@
+document.getElementById("notsign").addEventListener("click", function() {
+  alert("You need sign in first!");
+});
+// document.getElementById("notsign").addEventListener("click",needsignin());
+// function needsignin(){
+//   alert("đăng nhập trước đã");
+// }
+document.getElementById("buy").addEventListener("click", function() {
+  alert("buy sth !");
+});
+
